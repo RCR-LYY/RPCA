bc8acc1f66ce54af6bf7dd316a2032d51958bc78
a1657884486 <57613039+a1657884486@users.noreply.github.com>
2023-05-04 15:26:51 +0800

    Update GIP.py

--- GIP.py
+++ GIP.py
@@ -4,11 +4,9 @@ import math
 import ipdb
 
 def GIP_sm(A):
-    # 求权重Y_sm-行
     w1 = np.linalg.norm(A,axis=1)
-    widthSum = np.sum(np.square(w1))#该值为664
-    Y_sm = A.shape[0]/widthSum#该值为831/664
-    #构造GIP核相似性矩阵
+    widthSum = np.sum(np.square(w1))
+    Y_sm = A.shape[0]/widthSum
     G = np.zeros((A.shape[0],A.shape[0]))
     for i in range(G.shape[0]):
         for j in range(i, G.shape[1]):
@@ -17,11 +15,9 @@ def GIP_sm(A):
     return G
 
 def GIP_m(A):
-    # 求权重Y_m-列
     w1 = np.linalg.norm(A,axis=0)
     widthSum = np.sum(np.square(w1))
     Y_m = A.shape[1]/widthSum
-    #构造GIP核相似性矩阵
     G = np.zeros((A.shape[1],A.shape[1]))
     for i in range(G.shape[0]):
         for j in range(i, G.shape[1]):
@@ -35,28 +31,22 @@ def InSm(sm1, sm2, w):
     return simm
 
 def CaT_GIP(w):
-    # A = np.loadtxt(r"D:\A论文编辑\数据集及代码\dataset1\SM-miRNA关联矩阵.txt", dtype=int)
-    # SM = np.loadtxt(r"D:\A论文编辑\数据集及代码\dataset1\SM相似性矩阵.txt",dtype=float)
-    # miRNA = np.loadtxt(r"D:\A论文编辑\数据集及代码\dataset1\miRNA相似性矩阵.txt",dtype=float)
-    A = np.loadtxt(r"D:\A论文编辑\数据集及代码\dataset2\SM-miRNA association matrix 2.txt", dtype=int)
-    SM = np.loadtxt(r"D:\A论文编辑\数据集及代码\dataset2\SM相似性矩阵2.txt",dtype=float)
-    miRNA = np.loadtxt(r"D:\A论文编辑\数据集及代码\dataset2\miRNA相似性矩阵2.txt",dtype=float)
+    w = 0.2
+
+    A = np.loadtxt(r"SM-miRNA association matrix.txt", dtype=int)
+    SM = np.loadtxt(r"SM similarity matrix.txt",dtype=float)
+    miRNA = np.loadtxt(r"miRNA similarity matrix.txt",dtype=float)
+
     B = GIP_sm(A)
     C = GIP_m(A)
-    #相似性融合
+
     SM = InSm(copy.deepcopy(SM),copy.deepcopy(B),w)
     miRNA = InSm(copy.deepcopy(miRNA),copy.deepcopy(C),w)
-    #相似性保存
-    # np.savetxt(r'D:\A论文编辑\数据集及代码\dataset1\SM-GIP相似性矩阵.txt',SM,fmt='%e')
-    # np.savetxt(r'D:\A论文编辑\数据集及代码\dataset1\miRNA-GIP相似性矩阵.txt',miRNA,fmt='%e')
-    np.savetxt(r'D:\A论文编辑\数据集及代码\dataset2\SM-GIP相似性矩阵2.txt',SM,fmt='%e')
-    np.savetxt(r'D:\A论文编辑\数据集及代码\dataset2\miRNA-GIP相似性矩阵2.txt',miRNA,fmt='%e')
-    #矩阵拼接
+
     hs1 = np.hstack((SM,A))
     hs2 = np.hstack((np.transpose(A),miRNA))
     vs1 = np.vstack((hs1,hs2))
-    #拼接矩阵保存
-    # np.savetxt(r'D:\A论文编辑\数据集及代码\dataset1\T-GIP1.txt', vs1, fmt='%e')
-    np.savetxt(r'D:\A论文编辑\数据集及代码\dataset2\T-GIP2.txt',vs1,fmt='%e')
+
+    np.savetxt(r'T-GIP.txt', vs1, fmt='%e')
 
 
